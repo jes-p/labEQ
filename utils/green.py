dc006646a2152b907b16a1061a48a71fec27bf4d
Jes Parker <jes.prkr@gmail.com>
2021-10-07 13:29:16 -0700

    rewrote force option in get_greens, not yet tested

--- utils/green.py
+++ utils/green.py
@@ -2,24 +2,27 @@
 import matlab.engine
 import h5py
 
-def get_greens(dists, post_len, vp=2.74, vs=1.4):
-    """Version 0.0.0, highly unstable. Currently gets or makes GF for BP3 at 40Mhz only."""
+def get_greens(dists, post_len, vp=2.74, vs=1.4, source_type='moment',path = '/home/jes/data/Lab_Data/BP3_GF/'):
+    """Version 0.0.0, highly unstable. Currently gets or makes GF for BP3 at 40Mhz only.
+    Get or compute greens functions. Uses parallel processing in matlab to compute new GF. Start a matlab session and parallel pool separately, then call matlab.engine.ShareEngine to save ~20s per call of this function."""
     
     green = {}
     calc_dists = []
+    path += source_type + '/'
     for d in dists:
+        # check for existing GF.mat files for each distance
+        d_split = str(d).split('.')
+        if int(d_split[1]) == 0: d_split[1] = '' # all .00... outputs are the same
+        fname = f'{d_split[0]}_{d_split[1]}.mat'
         try:
-            # check for existing GF.mat files for each distance
-            d_split = str(d).split('.')
-            fname = f'{d_split[0]}_{d_split[1].mat}'
-            with h5py.File(fname,'r') as f:
+            with h5py.File(path+fname,'r') as f:
                 # check length
                 if f['Green']['post_len'] < post_len: raise EOFError('Not enough points in file')
                 # if still here then long enough so read it in
-                mdict = {}
-                for k,v in f['Green']['m']:
-                    mdict[k] = v
-                green[dist] = mdict
+                gdict = {}
+                for k,v in f['Green'][source_type[0]]:
+                    gdict[k] = v
+                green[dist] = gdict
             
         except:
             # add distance to list of dists to compute
@@ -27,15 +30,16 @@ def get_greens(dists, post_len, vp=2.74, vs=1.4):
             
     # calculate any new (or too short) dists
     if len(calc_dists) > 0:
-        with matlab.engine.start_engine() as eng:
-            eng.eval("addpath('/home/jes/Dropbox/Glaser_Lab/PlateSoln')")
+        with matlab.engine.connect_matlab() as eng:
+            eng.eval("addpath('/home/jes/dev/PlateSoln')")
             # set matlab variables for GF calc (can't just f-str them in)
             eng.workspace['dists'] = matlab.double([float(d) for d in calc_dists]) # dists in mm, import lists using the matlab.double constructor
             eng.workspace['cp'] = float(vp)
             eng.workspace['cs'] = float(vs)
             eng.workspace['post_len'] = float(post_len)
             # calculate all GF
-            done = eng.eval('mat_moment_GF(cp,cs,dists,post_len,"/home/jes/data/Lab_Data/BP3_GF/moment/")')
+            eval_string = f'mat_{source_type}_GF(cp,cs,dists,post_len,"{path}")'
+            done = eng.eval(eval_string)
     
     # make sure the function waited for matlab to finish
     assert done == 1
@@ -44,11 +48,12 @@ def get_greens(dists, post_len, vp=2.74, vs=1.4):
     # re-organize so this isn't repeated code
     for d in calc_dists:
         d_split = str(d).split('.')
+        if int(d_split[1]) == 0: d_split[1] = '' # all .00... outputs are the same
         fname = f'{d_split[0]}_{d_split[1].mat}'
-        with h5py.File(fname,'r') as f:
-            mdict = {}
-            for k,v in f['Green']['m']:
-                mdict[k] = v
-            green[dist] = mdict
+        with h5py.File(path+fname,'r') as f:
+            gdict = {}
+            for k,v in f['Green'][source_type[0]]:
+                gdict[k] = v
+            green[dist] = gdict
      
     return green
